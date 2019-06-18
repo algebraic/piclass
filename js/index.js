6548558ba967a8138f93176479db084e52416166
Johnson <JohnsonZ2@michigan.gov>
2019-06-18 16:27:11 -0400

    initial structure and stuff

---
+++ js/index.js
@@ -0,0 +1,34 @@
+$(function() {
+
+    $("section:not(:first)").each(function() {
+        var colors = ["#227675", "#3d85ab", "#a993e3", "#aa4bde", "#927f85", "#326753", "#7fb68f", "#2e4145", "#83eede", "#b67bb8"];
+        var random_color = colors[Math.floor(Math.random() * colors.length)];
+        $(this).css("background", random_color);
+    });
+
+    $("section").on({
+        mouseenter: function () {
+            $(this).html("<i class='fas fa-arrow-down'></i>");
+        },
+        mouseleave: function () {
+            $(this).text($(this).parents("section").attr("id"));
+        }
+    }, ".page-number"); //pass the element as an argument to .on
+
+    $("section").on("click", ".page-number", function() {
+        var $current = $(this).parents("section");
+        var $next = $("#" + (parseInt($current.attr("id"))+1));
+        $('html, body').stop().animate({scrollTop: $next[0].offsetTop}, 500);
+    });
+    $("section").on("click", ".up", function() {
+        var $current = $(this).parents("section");
+        var $next = $("#" + (parseInt($current.attr("id"))-1));
+        $('html, body').stop().animate({scrollTop: $next[0].offsetTop}, 500);
+    });
+
+
+    $("section:not(:first)").prepend("<div class='up controls'><i class='fas fa-arrow-up'></i></div>");
+    $("section").each(function() {
+        $(this).append("<div class='page-number controls'>" + $(this).attr("id") + "</div>");
+    });
+});
